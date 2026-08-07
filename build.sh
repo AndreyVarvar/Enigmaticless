4199594ee29fee401942a61f35e84be38ab76f07
Andrey Varvaryuk <andrey.varvaryuk@gmail.com>
2026-08-07 13:31:08 +0300

    added a new texture for survivor_scroll

--- build.sh
+++ build.sh
@@ -1,7 +1,34 @@
 #!/bin/zsh
 # this file will compile all the assets into a neat zip-package under build/ folder to be distributed
 
-if [[ $1 == "--help" || $1 == "-h" ]]; then
+show_help=false
+show_version=false
+bump_version=false
+bump_type="patch"
+directory="./build/"
+
+while getopts "hvbd:" opt; do
+  case "$opt" in
+  h) show_help=true ;;
+  v) show_version=true ;;
+  b)
+    bump_version=true
+    bump_type="patch"
+    ;;
+  B)
+    bump_version=true
+    bump_type="$OPTARG"
+    ;;
+  *)
+    echo "Invalid flag passed"
+    exit 1
+    ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
+if $show_help; then
   echo "NAME"
   echo "  build - compile all the assets in this directory into a single zip-file texture-pack"
   echo ""
@@ -12,7 +39,8 @@ if [[ $1 == "--help" || $1 == "-h" ]]; then
   echo "The build utility parses files in the location of invocation. If no option was passed, the script will compile all the assets and bump the version that can be specified as na argument similar to the ones for -b"
   echo "  -h, --help                      shows this help menu"
   echo "  -v, --version                   shows current version"
-  echo "  -b, --bump [major|minor|patch]  compiles and bumps version to a next one based on the argument. Defaults to a patch"
+  echo "  -b, --bump                      compiles and bumps patch version"
+  echo "  -B [major|minor|patch]          compiles and bumps version passed as an argument"
   echo ""
   echo "EXIT STATUS"
   echo "The build utility exits 0 on success, and 1 if an error occurs."
@@ -22,7 +50,7 @@ if [[ $1 == "--help" || $1 == "-h" ]]; then
   echo "    ./build.sh"
   echo "  will compile all the assets"
   echo "  The command:"
-  echo "    ./build.sh -b minor"
+  echo "    ./build.sh -B minor"
   echo "  will compile all the assets and bump the minor version"
   echo "  The command:"
   echo "    ./build.sh -b"
@@ -33,20 +61,18 @@ fi
 
 VERSION=$(cat pack-version.txt)
 
-if [[ $1 == "-v" || $1 == "--version" ]]; then
+if $show_version; then
   echo "Current version: $VERSION"
   exit 0
 fi
 
-if [[ $1 == "-b" || $1 == "--bump" ]]; then
-  BUMPTYPE="${2:-patch}"
-
+if $bump_version; then
   OLDVERSIONSUFFIX="${VERSION##*-}"
 
   local -a V
   V=(${(s:.:)OLDVERSIONSUFFIX})
 
-  case "$BUMPTYPE" in
+  case "$bump_type" in
   major)
     ((V[1]++))
     V[2]=0
@@ -60,7 +86,7 @@ if [[ $1 == "-b" || $1 == "--bump" ]]; then
     ((V[3]++))
     ;;
   *)
-    echo "Invalid argument: $BUMPTYPE" >&2
+    echo "Invalid argument: $bump_type" >&2
     exit 1
     ;;
   esac
